c35d0c6df3f3755b566274dea9775b4dd56d3358
falcon1969 <falcon.audia6@gmail.com>
2024-01-19 13:34:55 +0300

    added the public interfaces of app services (version 2)

---
+++ App/SportRadar.Api/Service.V2/IScoreBoardService.cs
@@ -0,0 +1,15 @@
+﻿using SportRadar.Api.Model;
+
+namespace SportRadar.Api.Service.V2
+{
+    public interface IScoreBoardService
+    {
+        Guid StartMatch(string homeTeamName, string awayTeamName);
+
+        void FinishMatch(Guid matchId);
+
+        void UpdateScore(Guid matchId, int homeTeamScore, int awayTeamScore);
+
+        IEnumerable<IScoreBoardItem> GetActiveMatchSummary();
+    }
+}
