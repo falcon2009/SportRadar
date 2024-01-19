f6849fd7999b345610d2f1856bb2e6a0730680dc
falcon1969 <falcon.audia6@gmail.com>
2024-01-19 13:32:07 +0300

    added the public interfaces of app services (version 1)

---
+++ App/SportRadar.Api/Service.V1/IMatchDispatcher.cs
@@ -0,0 +1,17 @@
+﻿using SportRadar.Api.Model;
+
+namespace SportRadar.Storage.Api.V1
+{
+    public interface IMatchDispatcher
+    {
+        Guid AnnonceMatch(string homeTeamName, string awayTeamName);
+
+        Guid StartMatch(Guid matchId);
+
+        Guid FinishMatch(Guid matchId);
+
+        Guid AddGoal(Guid matchId, string scoredTeamName);
+
+        IEnumerable<IMatchInfo> GetMatchInfoList();
+    }
+}
---
+++ App/SportRadar.Api/Service.V1/IScoreBoardDataProvider.cs
@@ -0,0 +1,9 @@
+﻿using SportRadar.Api.Model;
+
+namespace SportRadar.Api.Service.V1
+{
+    public interface IScoreBoardDataProvider
+    {
+        IEnumerable<IScoreBoardItem> GetActiveMatchSummary();
+    }
+}
