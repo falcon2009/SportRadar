ff5a9dbd92a33ec5669c6ec584235d3ac87544ff
falcon1969 <falcon.audia6@gmail.com>
2024-01-19 13:38:28 +0300

    added the storage entities models (version 1)

---
+++ App/SportRadar.Storage/Entity.V1/Match.cs
@@ -0,0 +1,9 @@
+﻿using Core.Api.Data;
+
+namespace SportRadar.Storage.Entity.V1
+{
+    public record Match(Guid Id, string HomeTeamName, string AwayTeamName, DateTime CreatedOn) : IEntity<Guid>
+    {
+        public Guid Key => this.Id;
+    }
+}
---
+++ App/SportRadar.Storage/Entity.V1/MatchEvent.cs
@@ -0,0 +1,10 @@
+﻿using Core.Api.Data;
+using SportRadar.Api.Enum;
+
+namespace SportRadar.Storage.Entity.V1
+{
+    public record MatchEvent(Guid Id, Guid MatchId, MatchEventType Type, DateTime CreatedOn, string? TeamName = null) : IEntity<Guid>
+    {
+        public Guid Key => this.Id;
+    }
+}
