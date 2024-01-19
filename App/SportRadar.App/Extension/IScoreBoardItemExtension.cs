ebe3422b7f251c8c772a08111daa088f9f6ee580
falcon1969 <falcon.audia6@gmail.com>
2024-01-19 14:06:17 +0300

    added IScoreBoardItem interface extension

---
+++ App/SportRadar.App/Extension/IScoreBoardItemExtension.cs
@@ -0,0 +1,17 @@
+﻿using SportRadar.Api.Model;
+
+namespace SportRadar.App.Extension
+{
+    public static class IScoreBoardItemExtension
+    {
+        public static string ToString(this IScoreBoardItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            return $"{item.HomeTeamName} {item.HomeTeamGoalTotal} - {item.AwayTeamName} {item.AwayTeamGoalTotal}";
+        }
+    }
+}
